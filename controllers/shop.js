3c021221f0f3bddc809be73d5c456e92f464b85a
Mohamed Fiyaz <124451741+Mohamed-Fiyaz@users.noreply.github.com>
2023-07-16 23:07:55 +0530

    Add files via upload

---
+++ controllers/shop.js
@@ -0,0 +1,163 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+  Product.findAll()
+  .then(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'Novels',
+      path: '/novels'
+    });
+  })
+  .catch(err => {
+    console.log(err)
+  })
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findByPk(prodId)
+  .then((product) => {
+    res.render('shop/product-detail', {
+      product: product, 
+      pageTitle: product.title,
+      path: '/detail'
+    });
+  })
+  .catch(err => {console.log(err)});
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.findAll()
+  .then(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Standalone Books',
+      path: '/'
+    });
+  })
+  .catch(err => {console.log(err)
+  })
+};
+
+exports.getCart = (req, res, next) => {
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart
+        .getProducts()
+        .then(products => {
+          let total = 0;
+          products.forEach(product => {
+            total += product.cartItem.quantity * product.price;
+          });
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products,
+            total: total
+          });
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+};
+
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId);
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user
+  .getCart()
+  .then(cart => {
+    return cart.getProducts({ where: { id: prodId} });
+  })
+  .then(products => {
+    const product = products[0];
+    return product.cartItem.destroy();
+  })
+  .then(result => {
+    res.redirect('/cart');
+  })
+  .catch(err => console.log(err));
+};
+
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user.getCart().then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts();
+  })
+  .then(products => {
+    return req.user
+    .createOrder()
+    .then(order => {
+      order.addProducts(products.map(product => {
+        product.orderItem = { quantity: product.cartItem.quantity };
+        return product;
+      }))
+    });
+  })
+  .then(result => {
+    return fetchedCart.setProducts(null);
+  })
+  .then(result =>{
+    res.redirect('/orders');
+  })
+  .catch(err => console.log(err));
+}
+
+exports.getOrders = (req, res, next) => {
+  req.user
+    .getOrders({ include: ['products'] })
+    .then(orders => {
+      // Calculate the total for each order
+      orders.forEach(order => {
+        let total = 0;
+        order.products.forEach(product => {
+          total += product.orderItem.quantity * product.price;
+        });
+        order.total = total;
+      });
+
+      res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders',
+        orders: orders
+      });
+    })
+    .catch(err => console.log(err));
+};
